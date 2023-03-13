89befb93100f41f9761e4d8c5a2d6748d7c99b3b
marianitadileo <mdileo36@gmail.com>
2023-03-13 01:55:33 +0100

    add exercise

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 //- scrivo nella console i numeri multipli di 3 e multipli di 5 stampi con le parole da stampare per ognuno dei multipli
 
 for (let i = 1; i < 101; i++) {
-    if (i % 3 == 0 && i % 5 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0) {
+        console.log("Buzz");
+    } else {
+    console.log(i);
+    } 
 }
+
+// ora creo la griglia con i numeri
+
+const numbGrid = document.querySelector(".grid");
+
+for (let i = 1; i < 101; i++) {
+    let box; 
+    if (i % 3 == 0 && i % 5 == 0) {
+        box = `<div class="box red"></div>`;
+    } else if (i % 3 == 0) {
+        box = `<div class="box green"></div>`; 
+    } else if (i % 5 == 0) {
+        box = `<div class="box yellow"></div>`;
+    } else { 
+        box = `<div class="box blue"></div>`;
+    }
+
+}
+
